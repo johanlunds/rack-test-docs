1a533d54599d0808e882bf2dac9fa30847a305f3
Johan Lundström <johan.lundstrom@fishbrain.com>
2015-08-07 01:19:51 +0200

    add basic test data

--- spec/lib/rack_test_docs/rspec_formatter_spec.rb
+++ spec/lib/rack_test_docs/rspec_formatter_spec.rb
@@ -12,12 +12,85 @@ describe RackTestDocs::RSpecFormatter do
 
   let(:output)               { StringIO.new }
   let(:formatter)            { RackTestDocs::RSpecFormatter.new(output) }
-  let(:example)              { RSpec::Core::ExampleGroup.describe.example }
-  let(:example_count)        { 2 }
-  let(:start_notification)   { RSpec::Core::Notifications::StartNotification.new(example_count, Time.now) }
+  let(:example_group)        do
+    group = RSpec::Core::ExampleGroup.describe
+    group.metadata.merge!({
+      :execution_result=>nil, # #<RSpec::Core::Example::ExecutionResult:0x007fa96f8aa8c0>
+      :block=> nil, # #<Proc:0x007fa96f8aae60@/Users/johan_lunds/Documents/Kod/rack-test-docs/spec/fixtures/rails_app/spec/requests/posts_requests_spec.rb:3>
+      :description_args=>["Posts REST API"],
+      :description=>"Posts REST API",
+      :full_description=>"Posts REST API",
+      :described_class=>nil,
+      :file_path=>"./spec/requests/posts_requests_spec.rb",
+      :line_number=>3,
+      :location=>"./spec/requests/posts_requests_spec.rb:3",
+      :absolute_file_path=>
+       "/Users/johan_lunds/Documents/Kod/rack-test-docs/spec/fixtures/rails_app/spec/requests/posts_requests_spec.rb",
+      :rerun_file_path=>"./spec/requests/posts_requests_spec.rb",
+      :scoped_id=>"1",
+      :type=>:request}
+      )
+    group
+  end
+  let(:sub_example_group) do
+    group = example_group.describe
+    group.metadata.merge!({
+      :execution_result=> nil , # #<RSpec::Core::Example::ExecutionResult:0x007fe14e181988>
+      :block=> nil, # #<Proc:0x007fe14e181be0@/Users/johan_lunds/Documents/Kod/rack-test-docs/spec/fixtures/rails_app/spec/requests/posts_requests_spec.rb:4>
+      :description_args=>["GET /posts"],
+      :description=>"GET /posts",
+      :full_description=>"Posts REST API GET /posts",
+      :described_class=>nil,
+      :file_path=>"./spec/requests/posts_requests_spec.rb",
+      :line_number=>4,
+      :location=>"./spec/requests/posts_requests_spec.rb:4",
+      :absolute_file_path=>
+       "/Users/johan_lunds/Documents/Kod/rack-test-docs/spec/fixtures/rails_app/spec/requests/posts_requests_spec.rb",
+      :rerun_file_path=>"./spec/requests/posts_requests_spec.rb",
+      :scoped_id=>"1:1",
+      :type=>:request,
+      :parent_example_group=> example_group.metadata}
+      )
+    group
+  end
+  let(:example) do
+    example = sub_example_group.example
+    example.metadata.merge!({
+      :execution_result=>nil, # #<RSpec::Core::Example::ExecutionResult:0x007fa96f9bc7b8>
+      :block=> nil,  # #<Proc:0x007fa96f9bca60@/Users/johan_lunds/Documents/Kod/rack-test-docs/spec/fixtures/rails_app/spec/requests/posts_requests_spec.rb:5>
+      :description_args=>["returns all posts"],
+      :description=>"returns all posts",
+      :full_description=>"Posts REST API GET /posts returns all posts",
+      :described_class=>nil,
+      :file_path=>"./spec/requests/posts_requests_spec.rb",
+      :line_number=>5,
+      :location=>"./spec/requests/posts_requests_spec.rb:5",
+      :absolute_file_path=>
+       "/Users/johan_lunds/Documents/Kod/rack-test-docs/spec/fixtures/rails_app/spec/requests/posts_requests_spec.rb",
+      :rerun_file_path=>"./spec/requests/posts_requests_spec.rb",
+      :scoped_id=>"1:1:1",
+      :type=>:request,
+      :example_group => sub_example_group.metadata,
+      :shared_group_inclusion_backtrace => [],
+      :last_run_status=>"unknown"}
+      )
+    example
+  end
+  let(:start_notification)   { RSpec::Core::Notifications::StartNotification.new(count: 2, load_time: 1.898042) }
+  let(:group_notification)   { RSpec::Core::Notifications::GroupNotification.new(group: example_group) }
+  let(:sub_group_notification)   { RSpec::Core::Notifications::GroupNotification.new(group: sub_example_group) }
   let(:example_notification) { RSpec::Core::Notifications::ExampleNotification.for(example) }
   let(:pending_notification) { RSpec::Core::Notifications::ExampleNotification.for(pending_example) }
   let(:failed_notification)  { RSpec::Core::Notifications::ExampleNotification.for(failed_example) }
+  let(:stop_notification)  do
+    # TODO
+    # RSpec::Core::Notifications::ExamplesNotification
+  end
+  let(:summary_notification)  do
+    # TODO
+    # RSpec::Core::Notifications::SummaryNotification
+  end
+  let(:close_notification) { RSpec::Core::Notifications::NullNotification.new }
 
   let(:failed_example) do
     exception = RuntimeError.new('Test Error')
@@ -25,9 +98,9 @@ describe RackTestDocs::RSpecFormatter do
       "/my/filename.rb:4:in `some_method'",
     ]
 
-    example = RSpec::Core::ExampleGroup.describe.example
+    example = sub_example_group.example
 
-    example.metadata[:file_path] = '/my/example/spec.rb'
+    example.metadata[:file_path] = './spec/requests/posts_requests_spec.rb'
     example.metadata[:execution_result].status = :failed
     example.metadata[:execution_result].exception = exception
 
@@ -35,7 +108,7 @@ describe RackTestDocs::RSpecFormatter do
   end
 
   let(:pending_example) do
-    example = RSpec::Core::ExampleGroup.describe.example
+    example = sub_example_group.example
     example.metadata[:execution_result].pending_fixed = true
     example
   end
